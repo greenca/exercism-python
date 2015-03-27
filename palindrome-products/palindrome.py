fc1e897ebea8ac2c49db43ef3fa77f47ad08fa0d
green <green@cganalytics.org>
2015-03-26 22:58:30 -0700

    faster palindrome products solution

--- palindrome-products/palindrome.py
+++ palindrome-products/palindrome.py
@@ -1,16 +1,34 @@
 def smallest_palindrome(max_factor, min_factor=0):
-    return palindrome(max_factor, min_factor, False)
+    cur_min = max_factor**2 + 1
+    for f1 in range(min_factor, max_factor + 1):
+        for f2 in range(f1, max_factor + 1):
+            if f1**2 > cur_min:
+                break  # don't waste time on factors that won't be minimums
+            product = f1*f2
+            if is_palindrome(product) and product < cur_min:
+                cur_min = product
+                cur_f1 = f1
+                cur_f2 = f2
+    if cur_min > max_factor**2:
+        return ()
+    else:
+        return (cur_min, (cur_f1, cur_f2))
 
 def largest_palindrome(max_factor, min_factor=0):
-    return palindrome(max_factor, min_factor, True)
-
-def palindrome(max_factor, min_factor, reverse_flag):
-    products = [(f1*f2, (f1, f2)) for f1 in range(min_factor, max_factor+1) for f2 in range(min_factor, f1+1)]
-    sorted_prod = sorted(products, key=lambda tup: tup[0], reverse=reverse_flag)
-    for tup in sorted_prod:
-        if is_palindrome(tup[0]):
-            return tup
-    return ()
+    cur_max = 0
+    for f1 in range(max_factor, min_factor - 1, -1):
+        for f2 in range(f1, min_factor - 1, -1):
+            if f1**2 < cur_max:
+                break  # don't waste time on factors that won't be maximums
+            product = f1*f2
+            if is_palindrome(product) and product > cur_max:
+                cur_max = product
+                cur_f1 = f1
+                cur_f2 = f2
+    if cur_max == 0:
+        return ()
+    else:
+        return (cur_max, (cur_f1, cur_f2))
 
 def is_palindrome(num):
     digits = list(str(num))
