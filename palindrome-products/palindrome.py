d0a279ea2e126c81a083bc59afd0dcaa03681d4f
green <green@cganalytics.org>
2015-03-26 23:25:53 -0700

    minor improvement

--- palindrome-products/palindrome.py
+++ palindrome-products/palindrome.py
@@ -1,9 +1,9 @@
 def smallest_palindrome(max_factor, min_factor=0):
     cur_min = max_factor**2 + 1
     for f1 in range(min_factor, max_factor + 1):
+        if f1**2 > cur_min:
+            break  # don't waste time on factors that won't be minimums
         for f2 in range(f1, max_factor + 1):
-            if f1**2 > cur_min:
-                break  # don't waste time on factors that won't be minimums
             product = f1*f2
             if is_palindrome(product) and product < cur_min:
                 cur_min = product
@@ -17,9 +17,9 @@ def smallest_palindrome(max_factor, min_factor=0):
 def largest_palindrome(max_factor, min_factor=0):
     cur_max = 0
     for f1 in range(max_factor, min_factor - 1, -1):
+        if f1**2 < cur_max:
+            break  # don't waste time on factors that won't be maximums
         for f2 in range(f1, min_factor - 1, -1):
-            if f1**2 < cur_max:
-                break  # don't waste time on factors that won't be maximums
             product = f1*f2
             if is_palindrome(product) and product > cur_max:
                 cur_max = product
