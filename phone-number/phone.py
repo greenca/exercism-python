9eac3816f8da0a2ff8b8c07ff1f978f9668190bc
green <green@cganalytics.org>
2015-04-16 14:03:55 -0700

    phone number exercise

---
+++ phone-number/phone.py
@@ -0,0 +1,16 @@
+class Phone:
+    
+    number = '0000000000'
+
+    def __init__(self, numStr):
+        numStr = numStr.translate(None, ' ()-.')
+        if len(numStr) == 10:
+            self.number = numStr
+        if len(numStr) == 11 and numStr[0] == '1':
+            self.number = numStr[1:]
+
+    def area_code(self):
+        return self.number[:3]
+
+    def pretty(self):
+        return '(' + self.number[:3] + ') ' + self.number[3:6] + '-' + self.number[6:]
